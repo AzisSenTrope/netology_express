bf1f61453d54cb84e6d02596f47a0d5d8e02a365
Belykh Yaroslav <orinnov.orin@yandex.ru>
2023-04-16 14:53:16 +0300

    Fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const runServer = require('./server/server');
 
 const PORT = process.env.PORT || 3002
-const UrlDB = process.env.UrlDB || 'mongodb://root:example@mongo:27017/';
+const urlDB = process.env.URL_DB || 'mongodb://root:example@mongo:27017/';
 
-runServer(PORT, UrlDB);
+runServer(PORT, urlDB);
